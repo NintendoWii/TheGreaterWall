52639766d5bbc93df08b5afabbb131f2b05aaed9
NintendoWii <kyle.s.desjardins1@gmail.com>
2021-09-09 17:59:26 +0000

    Add files via upload

---
+++ Modules/HostCollection/ActiveDirectoryEnumeration/ActiveDirectoryEnumeration.psm1
@@ -0,0 +1,89 @@
+function ActiveDirectoryEnumeration{
+    Remove-Variable -name properties -Force -ErrorAction SilentlyContinue
+
+    function build-properties ($accountnames){
+        $props= @()
+        foreach ($a in $accountnames){
+            $info= get-ADUser -Identity $a -Properties * | select * | ConvertTo-Csv | convertfrom-csv
+            $prop= $($info | Get-Member | where {$_.membertype -eq "NoteProperty"}).name
+            $props+= $prop
+
+            $props= $props | sort -Unique
+        }           
+                    
+        $props+= "Groups"
+        $props= $props | where {$_ -ne "RunspaceID" -and $_ -ne "ObjectGUID"}
+
+        return $props
+    }
+
+    function build-class($properties){
+        $date= (Get-Date -Format "dd-MMM-yyyy HH:mm").Split(":") -join ""
+        $outputclass= [pscustomobject][ordered]@{
+        Hostname= $($env:computername)
+        DateCollected= $date
+        }
+       
+        foreach ($p in $properties){
+            if ($p){
+                $outputclass | Add-Member -NotePropertyName $p -NotePropertyValue "NULL"
+            }
+        }
+        
+    return $outputclass
+   } 
+
+
+
+    #make persistent connection to DC
+    #get-pssession -name dcsesh -ErrorAction SilentlyContinue | remove-pssession
+    #$dcsesh= New-PSSession -name dcsesh -ComputerName $domaincontrollerip -Credential $dccreds
+    #invoke-command -Session $dcsesh -ScriptBlock {import-module activedirectory}
+    #Import-PSSession -Session $dcsesh -Module activedirectory -AllowClobber | out-null
+
+        #Get all AD info
+        $Everything= Get-ADUser -Filter * 
+
+        $Accountnames=@()
+        $finaloutput= @()
+        
+        foreach($i in $Everything){   
+            $Accountnames += $i.SamAccountName  
+        }           
+
+        $Accountnames= $Accountnames[50..56]
+
+        #Check to see if property list has been built out. If not, build it.
+        if (!$properties){
+            $properties= build-properties $accountnames
+        }
+           
+        foreach ($i in $Accountnames){
+            #pull all info
+            $results= build-class $properties
+            $output= get-ADUser -Identity $i -Properties * | select * | convertto-csv | convertfrom-csv        
+            
+            $resultspropertylist= $($results | Get-Member | where {$_.membertype -eq "Noteproperty"}).name
+            $resultspropertylist= $resultspropertylist | where {$_ -ne "IP" -and $_ -ne "Hostname" -and $_ -ne "DateCollected"}
+
+            foreach ($r in $resultspropertylist){
+                if ($output.$r){
+                    $results.$r = $output.$r
+                }
+            }
+
+            #get groups
+            $Groups= (Get-ADPrincipalGroupMembership "$i").name-join(',')
+            
+            #append groups to refned output
+            if ($groups){
+                $results.groups = $groups
+            }
+            
+            $finaloutput+= $results | convertto-json
+        }
+    
+        $finaloutput | ConvertFrom-Json | convertto-csv -NoTypeInformation
+    }
+
+Export-ModuleMember -Function ActiveDirectoryEnumeration
