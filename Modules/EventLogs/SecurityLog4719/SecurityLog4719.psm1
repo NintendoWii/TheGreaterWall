294487e439f12c1488222de42d63ef95d30f4d45
NintendoWii <kyle.s.desjardins1@gmail.com>
2022-01-30 21:36:22 +0300

    Add files via upload

--- Modules/EventLogs/SecurityLog4719/SecurityLog4719.psm1
+++ Modules/EventLogs/SecurityLog4719/SecurityLog4719.psm1
@@ -4,6 +4,7 @@ function SecurityLog4719{
             IP= "null"
             Hostname= $null
             DateCollected= $null
+            Source= "SecurityLog4719"
             InstaceID= $null
             Index= $null
             TimeGenerated= $null
