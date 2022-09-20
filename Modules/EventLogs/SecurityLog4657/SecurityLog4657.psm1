e1b6856c79648c98cf33754de6886c275d8cfe9e
NintendoWii <kyle.s.desjardins1@gmail.com>
2022-09-20 13:03:50 +0000

    Update SecurityLog4657.psm1

--- Modules/EventLogs/SecurityLog4657/SecurityLog4657.psm1
+++ Modules/EventLogs/SecurityLog4657/SecurityLog4657.psm1
@@ -3,6 +3,7 @@ function SecurityLog4657{
         $outputclass= [pscustomobject][ordered]@{
             IP= "null"
             Hostname= $null
+            OperatingSystem= $null
             DateCollected= $null
             Source= "SecurityLog4657"
             InstaceId= $null
@@ -23,6 +24,7 @@ function SecurityLog4657{
 
     $EventId4657 = Get-EventLog -LogName Security | Where-Object {$_.EventID -eq "4657"}
     $hostname= $env:COMPUTERNAME
+    $operatingsystem= $(Get-WmiObject win32_operatingsystem).name.tostring().split('|')[0]
     $date= (Get-Date -Format "dd-MMM-yyyy HH:mm").Split(":") -join ""
 
     foreach ($i in $EventId4657){
@@ -40,6 +42,7 @@ function SecurityLog4657{
         $P = $i.ReplacementStrings[12]
 
         $results.Hostname= $hostname
+        $results.operatingsystem= $operatingsystem
         $results.DateCollected= $date
         $results.InstaceId= $InstanceId
         $results.Index= $Index
