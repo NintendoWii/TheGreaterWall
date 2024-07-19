be738db4f27bd1538add9843826c8fc24d43e32f
NintendoWii <kyle.s.desjardins1@gmail.com>
2024-07-19 15:12:19 -0400

    Update SecurityLog4698.psm1

--- Modules/EventLogs/SecurityLog4698/SecurityLog4698.psm1
+++ Modules/EventLogs/SecurityLog4698/SecurityLog4698.psm1
@@ -21,7 +21,8 @@ function SecurityLog4698{
 
     $EventId4698 = Get-EventLog -LogName Security | Where-Object {$_.EventID -eq "4698"}
     $hostname= $env:COMPUTERNAME
-    $operatingsystem= $(Get-WmiObject win32_operatingsystem).name.tostring().split('|')[0]
+    $os= Get-CimInstance -ClassName Win32_OperatingSystem   
+    $operatingsystem= "$($os.caption) $($osversion)"
     $date= (Get-Date -Format "dd-MMM-yyyy HH:mm").Split(":") -join ""
 
     foreach ($i in $EventId4698){
