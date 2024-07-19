cef8fa12323622d40b7eb381b092f91c681e387f
NintendoWii <kyle.s.desjardins1@gmail.com>
2024-07-19 15:15:39 -0400

    Update SystemLog7036.psm1

--- Modules/EventLogs/SystemLog7036/SystemLog7036.psm1
+++ Modules/EventLogs/SystemLog7036/SystemLog7036.psm1
@@ -20,7 +20,8 @@ function SystemLog7036{
 
     $EventId7036 = Get-EventLog -LogName System | Where-Object {$_.EventID -eq "7036"}
     $hostname= $env:COMPUTERNAME
-    $operatingsystem= $(Get-WmiObject win32_operatingsystem).name.tostring().split('|')[0]
+    $os= Get-CimInstance -ClassName Win32_OperatingSystem   
+    $operatingsystem= "$($os.caption) $($osversion)"
     $date= (Get-Date -Format "dd-MMM-yyyy HH:mm").Split(":") -join ""
 
     foreach ($i in $EventId7036){
