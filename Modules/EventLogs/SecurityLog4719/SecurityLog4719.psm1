5042c79bc81dc79d4b75c01b1192d1c8e6cd7213
NintendoWii <kyle.s.desjardins1@gmail.com>
2024-07-19 15:12:33 -0400

    Update SecurityLog4719.psm1

--- Modules/EventLogs/SecurityLog4719/SecurityLog4719.psm1
+++ Modules/EventLogs/SecurityLog4719/SecurityLog4719.psm1
@@ -23,7 +23,8 @@ function SecurityLog4719{
 
     $EventId4719 = Get-EventLog -LogName Security | Where-Object {$_.EventID -eq "4719"}
     $hostname= $env:COMPUTERNAME
-    $operatingsystem= $(Get-WmiObject win32_operatingsystem).name.tostring().split('|')[0]
+    $os= Get-CimInstance -ClassName Win32_OperatingSystem   
+    $operatingsystem= "$($os.caption) $($osversion)"
     $date= (Get-Date -Format "dd-MMM-yyyy HH:mm").Split(":") -join ""
 
     foreach ($i in $EventId4719){
