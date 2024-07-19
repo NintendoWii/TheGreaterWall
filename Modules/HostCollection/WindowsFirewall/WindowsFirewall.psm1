8a1d1d680a8c1a02083ed3b3510fde9bb59ec130
NintendoWii <kyle.s.desjardins1@gmail.com>
2024-07-19 15:10:02 -0400

    Update WindowsFirewall.psm1

--- Modules/HostCollection/WindowsFirewall/WindowsFirewall.psm1
+++ Modules/HostCollection/WindowsFirewall/WindowsFirewall.psm1
@@ -25,7 +25,8 @@ function WindowsFirewall{
     $output= @()
 
     $hostname= $env:COMPUTERNAME
-    $operatingsystem= $(Get-WmiObject win32_operatingsystem).name.tostring().split('|')[0]
+    $os= Get-CimInstance -ClassName Win32_OperatingSystem   
+    $operatingsystem= "$($os.caption) $($osversion)"
     $date= (Get-Date -Format "dd-MMM-yyyy HH:mm").Split(":") -join ""
 
 
