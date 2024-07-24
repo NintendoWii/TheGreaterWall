b2da89ebb0192086400f15ad21a77f5d25a70676
NintendoWii <kyle.s.desjardins1@gmail.com>
2024-07-24 12:46:11 -0400

    Update Prefetch.psm1

--- Modules/HostCollection/Prefetch/Prefetch.psm1
+++ Modules/HostCollection/Prefetch/Prefetch.psm1
@@ -20,7 +20,7 @@ function Prefetch{
     
     $hostname= $env:COMPUTERNAME  
     $os= Get-CimInstance -ClassName Win32_OperatingSystem   
-    $operatingsystem= "$($os.caption) $($osversion)"
+    $operatingsystem= "$($os.caption) $($os.version)"
     $date= (Get-Date -Format "dd-MMM-yyyy HH:mm").Split(":") -join ""
 
     foreach ($p in $Prefetch){
